ceeaf33b416cdaba64265497683fb591c455ee63
kissapongs@gmail.com <KSPSSW>
2024-07-09 14:27:40 +0700

    first commit

---
+++ src/main/java/com/mycompany/xov2/XOv2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xov2;
+
+/**
+ *
+ * @author informatics
+ */
+public class XOv2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
